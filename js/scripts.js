98652ab25c53c92e27e62e30c475ce13e0cd4001
Clifford Grimmell and Jahan Walsh <clifford@>
2017-02-15 13:51:42 -0800

    fix bug that caused information to fail to appear

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
-$(function(){
-  $("#formone").submit(function(event){
+
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    debugger;
     var person1Input = $("input#person1").val();
     var dateInput = $("input#date").val();
-    var startInput = $("input#start-time");
-    var endInput = $("input#end-time");
-    console.log("person1Input" + "dateInput" + "startInput" + "endInput");
+    var startInput = $("input#startTime").val();
+    var endInput = $("input#endTime").val();
 
     $(".person1").text(person1Input);
     $(".date").text(dateInput);
-    $(".start-time").text(startInput);
-    $(".end-time").text(endInput);
-    $("#confirm").text();
+    $(".startTime").text(startInput);
+    $(".endTime").text(endInput);
 
     $(".panel").show();
     event.preventDefault();
